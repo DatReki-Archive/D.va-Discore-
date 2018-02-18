0696904e7cecb2b8b65515a0594afd90776ac8e0
Timo <31971873+DatReki@users.noreply.github.com>
2018-02-18 20:01:27 +0100

    Create Info.cs

---
+++ D.va/Commands/Info.cs
@@ -0,0 +1,32 @@
+using D.va.Basic;
+using Discore;
+using Discore.WebSocket;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace D.va.Commands
+{
+
+    class InfoCommand : Command
+    {
+        public InfoCommand() : base()
+        {
+            Name = "info";
+            Desc = "Information about the bot";
+        }
+        public override async void Execute(ITextChannel Channel, MessageEventArgs e, MatchCollection args)
+        {
+
+            try
+            {
+                await Channel.TriggerTypingIndicator();
+                await Channel.CreateMessage($"Hello I'm D.va and I'm created by {BotOwner.BotOwnerName + BotOwner.BotOwnerDiscrim}\nOwner ID: {BotOwner.BotOwnerID}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}

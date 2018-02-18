95874edb349e354a807c8bd4bba15d5834cbba2c
Timo <31971873+DatReki@users.noreply.github.com>
2018-02-18 20:09:03 +0100

    Create BotOwner.cs

---
+++ D.va/Basic/BotOwner.cs
@@ -0,0 +1,9 @@
+namespace D.va.Basic
+{
+    public class BotOwner
+    {
+       public static string BotOwnerName = "TimoHalofan";
+       public static string BotOwnerDiscrim = "#7092";
+       public static string BotOwnerID = "176814930605899778";
+    }
+}

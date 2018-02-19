7bdb82cb29d55bae72154546ce6919cc206106f0
Aaron Pearson <aaron.2002@outlook.com>
2018-02-19 14:01:57 +0000

    Should do ServerID

--- D.va/Basic/BotOwner.cs
+++ D.va/Basic/BotOwner.cs
@@ -5,5 +5,6 @@ namespace D.va.Basic
        public static string BotOwnerName = "TimoHalofan";
        public static string BotOwnerDiscrim = "#7092";
        public static string BotOwnerID = "176814930605899778";
+       public static string BotOwnerServerID = "362665174433267722";
     }
 }
